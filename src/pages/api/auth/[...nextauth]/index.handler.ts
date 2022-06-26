a00fa52c6780a544d287b50b8c76c9ac2e76a79e
you-5805 <you.iwamoto918@gmail.com>
2022-06-27 01:38:59 +0900

    add nex-auth prisma adapter

--- src/pages/api/auth/[...nextauth]/index.handler.ts
+++ src/pages/api/auth/[...nextauth]/index.handler.ts
@@ -1,8 +1,11 @@
 import { GITHUB_OAUTH_CLIENT_ID, GITHUB_OAUTH_CLIENT_SECRET } from '@/config/const';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
 import nextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 
 const handler = nextAuth({
+  adapter: PrismaAdapter(new PrismaClient()),
   providers: [
     GitHubProvider({
       clientId: GITHUB_OAUTH_CLIENT_ID,

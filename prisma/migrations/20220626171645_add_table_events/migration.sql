f64f52d8e1461d6162871770b29177841c7d4ff3
you-5805 <you.iwamoto918@gmail.com>
2022-07-06 20:08:41 +0900

    rm dockerfile, cleanup migrations, use planetscale

--- prisma/migrations/20220626171645_add_table_events/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "events" (
-    "id" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "events_pkey" PRIMARY KEY ("id")
-);

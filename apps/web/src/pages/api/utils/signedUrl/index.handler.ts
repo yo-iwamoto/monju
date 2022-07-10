3be90b3f8db6f96a068215168aec5ef4189afe3e
you-5805 <you.iwamoto918@gmail.com>
2022-07-10 18:37:32 +0900

    rm unused files

--- apps/web/src/pages/api/utils/signedUrl/index.handler.ts
+++
@@ -1,9 +0,0 @@
-import { R2_JWT_SECRET } from '@/config/const';
-import { NextApiHandler } from 'next';
-import jwt from 'jsonwebtoken';
-
-const handler: NextApiHandler = (_req, res) => {
-  const signature = res.send(verified);
-};
-
-export default handler;

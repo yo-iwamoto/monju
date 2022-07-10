f48e02dc630531e30f2867940ab53ed25665153c
you-5805 <you.iwamoto918@gmail.com>
2022-07-10 18:39:15 +0900

    rm unused files

--- apps/web/src/config/const.ts
+++ apps/web/src/config/const.ts
@@ -1,8 +1,6 @@
 export const R2_ENDPOINT = process.env.R2_ENDPOINT as string;
 export const R2_ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID as string;
 export const R2_SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY as string;
-export const R2_DIRECT_UPLOAD_URL = process.env.R2_DIRECT_UPLOAD_URL as string;
-export const R2_JWT_SECRET = process.env.R2_JWT_SECRET as string;
 export const GITHUB_OAUTH_CLIENT_ID = process.env.GITHUB_OAUTH_CLIENT_ID as string;
 export const GITHUB_OAUTH_CLIENT_SECRET = process.env.GITHUB_OAUTH_CLIENT_SECRET as string;
 export const NODE_ENV = process.env.NODE_ENV;
--- apps/web/src/pages/index.view.tsx
+++ apps/web/src/pages/index.view.tsx
@@ -1,8 +1,6 @@
 import { Link } from '@/components/Link';
 import { pagesPath } from '@/lib/$path';
-import { Button } from '@/components/Button';
-import { ChangeEvent, FC, useCallback, useState } from 'react';
-import axios from 'axios';
+import { FC } from 'react';
 import type { Event } from '@/types/Resource';
 
 type Props = {
@@ -10,13 +8,6 @@ type Props = {
 };
 
 export const View: FC<Props> = ({ events }) => {
-  const [file, setFile] = useState<File | null>(null);
-  const onChangeFile = useCallback(({ target: { files } }: ChangeEvent<HTMLInputElement>) => {
-    if (!files?.length) return;
-
-    setFile(files[0]);
-  }, []);
-
   return (
     <>
       {events &&
@@ -25,16 +16,6 @@ export const View: FC<Props> = ({ events }) => {
             <p key={event.id}>{event.title}</p>
           </Link>
         ))}
-      <div className='h-20'></div>
-      <input type='file' accept='image/*' onChange={onChangeFile} />
-      <Button
-        onClick={async () => {
-          const res = await axios.put('https://r2-worker.yoiw-cw-demo.workers.dev/', file);
-          console.log(res.data);
-        }}
-      >
-        push!
-      </Button>
     </>
   );
 };

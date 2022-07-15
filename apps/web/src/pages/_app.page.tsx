8c6e0a263d932de020f8bd758e53c3c1e4a8bbf7
you-5805 <you.iwamoto918@gmail.com>
2022-07-15 20:31:43 +0900

    fix import order

--- apps/web/src/pages/_app.page.tsx
+++ apps/web/src/pages/_app.page.tsx
@@ -1,9 +1,9 @@
+import { fetcher } from '@/lib/swr/fetcher';
+import { Layout } from '@/components/Layout';
 import { SWRConfig } from 'swr';
 import '@/styles/global.css';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
-import { fetcher } from '@/lib/swr/fetcher';
-import { Layout } from '@/components/Layout';
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (

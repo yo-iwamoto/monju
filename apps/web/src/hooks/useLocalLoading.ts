4253a87fb58406cbb755735e91291f38ae2b79e1
you-5805 <you.iwamoto918@gmail.com>
2022-07-22 00:37:28 +0900

    fix: awaitable typing

--- apps/web/src/hooks/useLocalLoading.ts
+++ apps/web/src/hooks/useLocalLoading.ts
@@ -1,9 +1,10 @@
 import { useCallback, useState } from 'react';
+import type { Awaitable } from '@/types/utils';
 
 export const useLocalLoading = <T>(): [boolean, (cb: () => T | Promise<T>) => Promise<T>] => {
   const [isLoading, setIsLoading] = useState(false);
 
-  const load = useCallback(async (cb: () => T | Promise<T>) => {
+  const load = useCallback(async (cb: () => Awaitable<T>) => {
     setIsLoading(true);
     const result = await cb();
     setIsLoading(false);

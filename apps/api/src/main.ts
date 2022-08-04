455ec956b47d5ee27916244ee8b924accd70333c
Yo Iwamoto <you.iwamoto918@gmail.com>
2022-08-04 23:36:49 +0900

    add @monju/api app

---
+++ apps/api/src/main.ts
@@ -0,0 +1,19 @@
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyCookie from '@fastify/cookie';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.register(fastifyCookie, {
+    secret: process.env.COOKIE_SECRET,
+  });
+  await app.listen(8000);
+}
+bootstrap();
